e716a0a6689d273d42a85c34a9ba481b8055490e
Asxing Yang（阿行） <AsxingYang@gmail.com>
2021-04-27 22:50:10 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,9 +22,9 @@ git config --global user.email "$INPUT_USER_EMAIL"
 git config --global user.name "$INPUT_USER_NAME"
 git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://x-access-token:$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
 
-rm -rf $CLONE_DIR/$INPUT_DESTINATION_FOLDER
-mkdir -p $CLONE_DIR/$INPUT_DESTINATION_FOLDER
+rm -rf $CLONE_DIR/$INPUT_DESTINATION_FOLDER/*
 echo "Copying contents to git repo"
+mkdir -p $CLONE_DIR/$INPUT_DESTINATION_FOLDER
 cp -R "$INPUT_SOURCE_FILE" "$CLONE_DIR/$INPUT_DESTINATION_FOLDER"
 cd "$CLONE_DIR"
 
@@ -41,7 +41,11 @@ fi
 
 echo "Adding git commit"
 git add .
-
-git commit --message "$INPUT_COMMIT_MESSAGE"
-echo "Pushing git commit"
-git push -u origin HEAD:$OUTPUT_BRANCH
+if git status | grep -q "Changes to be committed"
+then
+  git commit --message "$INPUT_COMMIT_MESSAGE"
+  echo "Pushing git commit"
+  git push -u origin HEAD:$OUTPUT_BRANCH
+else
+  echo "No changes detected"
+fi
