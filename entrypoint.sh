c0bbbe0901808326375e88e642ee868f89ca2a53
Asxing Yang（阿行） <AsxingYang@gmail.com>
2021-04-27 21:33:13 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,14 +22,11 @@ git config --global user.email "$INPUT_USER_EMAIL"
 git config --global user.name "$INPUT_USER_NAME"
 git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://x-access-token:$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
 
-echo "rf -rf xxx"
 rm -rf $CLONE_DIR/$INPUT_DESTINATION_FOLDER
-echo "mkdir -p xxx"
 mkdir -p $CLONE_DIR/$INPUT_DESTINATION_FOLDER
 echo "Copying contents to git repo"
 cp -R "$INPUT_SOURCE_FILE" "$CLONE_DIR/$INPUT_DESTINATION_FOLDER"
-echo "cd ..."
-cd "$CLONE_DIR/$INPUT_DESTINATION_FOLDER"
+cd "$CLONE_DIR"
 
 if [ ! -z "$INPUT_DESTINATION_BRANCH_CREATE" ]
 then
@@ -45,14 +42,6 @@ fi
 echo "Adding git commit"
 git add .
 
-echo "path ....."
-pwd
-
-if [ -z "$(git status --porcelain)" ]
-then
-  echo "No changes detected"
-else
-  git commit --message "$INPUT_COMMIT_MESSAGE"
-  echo "Pushing git commit"
-  git push -u origin HEAD:$OUTPUT_BRANCH
-fi
+git commit --message "$INPUT_COMMIT_MESSAGE"
+echo "Pushing git commit"
+git push -u origin HEAD:$OUTPUT_BRANCH
