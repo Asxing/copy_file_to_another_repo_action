c5a67608588ea639437fded9ce7e3a9214d76303
Asxing Yang（阿行） <AsxingYang@gmail.com>
2021-04-29 21:57:07 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,17 +19,16 @@ CLONE_DIR=$(mktemp -d)
 
 echo "Cloning destination git repository"
 
-git config -l | grep 'http\..*\.extraheader' | cut -d= -f1 | \             # dike out the http.extraheader from git config
-            xargs -L1 git config --unset-all
 
-git config --global user.email "$INPUT_USER_EMAIL"
-git config --global user.name "$INPUT_USER_NAME"
 git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://x-access-token:$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
 rm -rf $CLONE_DIR/$INPUT_DESTINATION_FOLDER/*
 echo "Copying contents to git repo"
 mkdir -p $CLONE_DIR/$INPUT_DESTINATION_FOLDER
 cp -R "$INPUT_SOURCE_FILE"/* "$CLONE_DIR/$INPUT_DESTINATION_FOLDER"
 cd "$CLONE_DIR"
+
+git config --local user.email "$INPUT_USER_EMAIL"
+git config --local user.name "$INPUT_USER_NAME"
 
 if [ ! -z "$INPUT_DESTINATION_BRANCH_CREATE" ]
 then
