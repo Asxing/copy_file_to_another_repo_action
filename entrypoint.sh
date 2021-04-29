b7eca23ab3b5324aa31a50944d84526ed53c8e41
Asxing Yang（阿行） <AsxingYang@gmail.com>
2021-04-29 19:46:21 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,8 +18,16 @@ OUTPUT_BRANCH="$INPUT_DESTINATION_BRANCH"
 CLONE_DIR=$(mktemp -d)
 
 echo "Cloning destination git repository"
-git config --global user.email "$INPUT_USER_EMAIL"
-git config --global user.name "$INPUT_USER_NAME"
+if [ -z "$INPUT_USER_EMAIL" ]
+then
+  git config --global user.email "$INPUT_USER_EMAIL"
+fi
+
+if [ -z "$INPUT_USER_NAME" ]
+then
+  git config --global user.name "$INPUT_USER_NAME"
+fi
+
 git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://x-access-token:$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
 
 rm -rf $CLONE_DIR/$INPUT_DESTINATION_FOLDER/*
